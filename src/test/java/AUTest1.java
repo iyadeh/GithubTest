bf2c6188464fcc5f4e5696b269c54ed6d3064d31
iyadeh <94737123+iyadeh@users.noreply.github.com>
2023-02-13 21:46:14 +0700

    Initial commit

---
+++ src/test/java/AUTest1.java
@@ -0,0 +1,75 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/EmptyTestNGTest.java to edit this template
+ */
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import static org.testng.Assert.*;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+/**
+ *
+ * @author arga.prayoga
+ */
+public class AUTest1 {
+    WebDriver driver = null;
+    
+    @BeforeTest
+    public void setUp(){
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+    }
+    
+    @Test
+    public void firstTry(){
+        driver.get("https://www.google.com");
+        driver.findElement(By.id("no")).click();
+        driver.findElement(By.id("buttoncheck")).click();
+        String expectedResult = "Radio button is checked and it's value is No";
+        String actualResult = driver.findElement(By.className("radiobutton")).getText();
+        Assert.assertEquals(actualResult, expectedResult);
+    }
+    
+    @AfterTest
+    public void tearDown(){
+        driver.close();
+        driver.quit();
+    }
+    public AUTest1() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @BeforeMethod
+    public void setUpMethod() throws Exception {
+    }
+
+    @AfterMethod
+    public void tearDownMethod() throws Exception {
+    }
+}
